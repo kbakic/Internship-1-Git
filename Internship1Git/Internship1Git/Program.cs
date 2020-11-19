97d4ac490ce631ea25f32124aa933cc847967bac
kbakic <pripravnik-karlo.bakic@dump.hr>
2020-11-19 20:42:50 +0100

    Zadnji commit

---
+++ Internship1Git/Internship1Git/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Internship1Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Zadnji commit");
+        }
+    }
+}
